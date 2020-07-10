833374c54e3a12dd193193d01eb3d43b069098e7
Jargal <jargal@kt.mn>
2020-07-10 21:53:08 +0800

    Гэртээ бичсэн тайлбар

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import NextApp from './NextApp';
-import registerServiceWorker from './registerServiceWorker';
+import NextApp from "./NextApp";
+import registerServiceWorker from "./registerServiceWorker";
 // Add this import:
-import {AppContainer} from 'react-hot-loader';
+import { AppContainer } from "react-hot-loader";
 
 // Wrap the rendering in a function:
-const render = Component => {
+const render = (Component) => {
   ReactDOM.render(
     // Wrap App inside AppContainer
     <AppContainer>
-      <Component/>
+      <Component />
     </AppContainer>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 };
+
+//Энд Жаргал гэртээ энэхүү comment-ийг бичлээ. 2020 оны 7 сарын 10 өдөр. Орой 21:52
 
 // Do this once
 registerServiceWorker();
@@ -25,7 +27,7 @@ render(NextApp);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./NextApp', () => {
+  module.hot.accept("./NextApp", () => {
     render(NextApp);
   });
 }
