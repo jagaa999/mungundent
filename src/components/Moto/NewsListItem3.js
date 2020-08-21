ce5a4aa6feddfcbcca8a79328138addf4c4c0737
Jargal Home <jargal@kt.mn>
2020-08-21 08:24:17 -0700

    table again

--- src/components/Moto/NewsListItem3.js
+++ src/components/Moto/NewsListItem3.js
@@ -128,36 +128,13 @@ const NewsItem = ({ newsItems }) => {
       title: "Огноо",
       dataIndex: "publisheddate",
       key: "publisheddate",
-      render: (publisheddate) => Moment(publisheddate, "DD MM YYYY"),
-      // publisheddate
+      render: (publisheddate) => (
+        Moment(publisheddate, "DD MM YYYY"), publisheddate
+      ),
     },
   ];
 
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["loser"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sidney No. 1 Lake Park",
-      tags: ["cool", "teacher"],
-    },
-  ];
-
-  return <Table columns={columns} dataSource={newsItems} />;
+  return <Table columns={columns} dataSource={newsItems} pagination={false} />;
 };
 
 export default NewsItem;
