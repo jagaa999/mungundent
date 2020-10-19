d70b608e9025138ea99aed175eb291f7685fe060
jagaa <jargal@kt.mn>
2020-10-19 18:23:25 +0800

    react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,32 +1,56 @@
 import { AppContainer } from "react-hot-loader";
 import React from "react";
-import ReactDOM from "react-dom";
+
+// https://www.npmjs.com/package/react-snap
+// import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 
 import NextApp from "./NextApp";
 import registerServiceWorker from "./registerServiceWorker";
 // Add this import:
 
 // Wrap the rendering in a function:
-const render = (Component) => {
-  ReactDOM.render(
-    // Wrap App inside AppContainer
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById("root")
-  );
+// const render = (Component) => {
+//   ReactDOM.render(
+//     // Wrap App inside AppContainer
+//     <AppContainer>
+//       <Component />
+//     </AppContainer>,
+//     document.getElementById("root")
+//   );
+// };
+
+// https://www.npmjs.com/package/react-snap
+const rootElement = document.getElementById("root");
+
+const myRender = (Component) => {
+  if (rootElement.hasChildNodes()) {
+    hydrate(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      rootElement
+    );
+  } else {
+    render(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      rootElement
+    );
+  }
 };
 
 // Do this once
 registerServiceWorker();
 
 // Render once
-render(NextApp);
+myRender(NextApp);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
   module.hot.accept("./NextApp", () => {
-    render(NextApp);
+    myRender(NextApp);
   });
 }
 
