7eb46a6f2cb0ad4115370db150e2e3c6571b85a0
Jargal Home <jargal@kt.mn>
2020-12-22 21:16:54 +0800

    compare item

--- src/components/Moto/CompareItems.js
+++ src/components/Moto/CompareItems.js
@@ -1,22 +1,130 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
-import { Row, Col, Tabs, Button, Table } from "antd";
+import { Row, Col, Tabs, Button, Table, Image, Tag, Tooltip } from "antd";
 import CompareContext from "context/CompareContext";
-import MotoSmartHomeCard2 from "../Widgets/MotoSmartHomeCard2";
-import MotoSmartHomeCard3 from "../Widgets/MotoSmartHomeCard3";
 
 const CompareItems = () => {
   const compareContext = useContext(CompareContext);
+
+  console.table(compareContext.compareList.compareList);
+
+  const myTable = {
+    AUCTION: ["MIRIVE Osaka", "TOKYO", "JAPAN"],
+    AUCTION_DATE: ["2020-12-24 11:40:00", "", "2020-12-25 15:10:00"],
+    AVG_PRICE: ["1903000", "2103590", "1098054"],
+    COLOR: ["silver", "", ""],
+  };
+
+  let myTable3 = {};
+  let myColumn3 = [
+    {
+      title: "Үзүүлэлт",
+      dataIndex: "label",
+    },
+  ];
+  const myDistinct = [];
+
+  compareContext.compareList.compareList.map((item, index) => {
+    const originalItem = item.originalItem;
+    const { image, link, mainSpec } = item;
+
+    const myItem = <Image src={item.image} />;
+
+    const myColItem = {
+      title: (
+        <div style={{ position: "relative", width: "100%" }}>
+          <Image src={item.image} />
+          <Tag color="warning" className="moto-badge-2-1">
+            {item.mainSpec}
+          </Tag>
+          <h5 className="gx-mt-2">
+            <Link to={item.link || "/"}>{item.title || "Тодорхойгүй"}</Link>
+          </h5>
+          <div className="gx-fs-sm gx-mt-2 gx-d-none gx-d-sm-block">
+            {item.subSpecs.map((subSpec, index) => (
+              <Tooltip title={subSpec.title} key={index}>
+                <span className="moto-label-main ant-tag">{subSpec.value}</span>
+              </Tooltip>
+            ))}
+          </div>
+        </div>
+      ),
+      dataIndex: ["item" + (index * 1 + 1)],
+    };
+    myColumn3.push(myColItem);
+
+    Object.keys(originalItem).map((ii, index) => {
+      if (!myDistinct.includes(ii)) {
+        myDistinct.push(ii);
+        myTable3 = { ...myTable3, [ii]: [] };
+      }
+    });
+  });
+
+  const columnTaa2 = [
+    {
+      title: "Үзүүлэлт",
+      dataIndex: "label",
+      key: "title",
+    },
+    {
+      title: "1",
+      dataIndex: "item1",
+      key: "item1",
+    },
+    {
+      title: "2",
+      dataIndex: "item2",
+      key: "item2",
+    },
+    {
+      title: "3",
+      dataIndex: "item3",
+      key: "item3",
+    },
+  ];
+
+  // console.log("myDistinct", myDistinct);
+  // console.log("myTable3", myTable3);
+
+  compareContext.compareList.compareList.map((item, index) => {
+    const originalItem = item.originalItem;
+
+    myDistinct.map((fff, index) => {
+      myTable3[fff].push(originalItem[fff] || "");
+    });
+  });
+
+  // console.log("AGAIN → myTable3", myTable3);
+
+  let myTaaa = [];
+  Object.keys(myTable3).map((item, index) => {
+    let myRow = { key: index, label: item };
+    myTable3[item].map((value, valueindex) => {
+      myRow = {
+        ...myRow,
+        ["item" + (valueindex * 1 + 1)]: value,
+      };
+    });
+
+    myTaaa.push(myRow);
+  });
+
+  // console.log("myTaaa", myTaaa);
 
   return (
     <>
       <h2>Харьцуулах</h2>
       <Button onClick={compareContext.toggleDrawer}>Харьцуулах </Button>
-      {compareContext.compareList.compareList.map((item, index) => (
-        <div key={index}>ddsf dsf ds</div>
-      ))}
 
-      <Table dataSource={compareContext.compareList.compareList} />
+      <Table
+        dataSource={myTaaa}
+        // columns={columnTaa2}
+        columns={myColumn3}
+        pagination={false}
+        tableLayout="fixed"
+      />
     </>
   );
 };
--- src/context/CompareContext.js
+++ src/context/CompareContext.js
@@ -20,57 +20,147 @@ export const CompareStore = (props) => {
 
   const [compareList, setCompareList] = useState({
     compareList: [
-      {
-        title: "2014 Toyota Harrier",
-        image:
-          "https://8.ajes.com/imgs/23FCi0VbnvRhXb4iHollYQZOsAFE37O8hY7Ix48a8C&w=320#https://8.ajes.com/imgs/23FCi0VbnvRhXb4iHollYQZOsAFE37O968IR6F4eEp&h=50",
-        mainSpec: "3.5",
-        link: "/auction/oTqyOK8MrAxdNQ8",
-        subSpecs: [
-          {
-            title: "Өнгө",
-            value: "Silver",
-          },
-          {
-            title: "Хөдөлгүүр",
-            value: "2500 cc",
-          },
-        ],
-      },
-      {
-        title: "2014 Toyota Harrier",
-        image:
-          "https://8.ajes.com/imgs/aKxY8uthGoTnVCYdz2ga1hRTLFvzLVor4ms1bIBEOyTspOf0dsb3&w=320#https://8.ajes.com/imgs/aKxY8uthGoTnVCYdz2ga1hRTLFvzLVor4ms1bIBEOyTspOqISQTv&h=50",
-        mainSpec: "3.5",
-        link: "/auction/oTqyOK8MrAxdNQ8",
-        subSpecs: [
-          {
-            title: "Өнгө",
-            value: "Silver",
-          },
-          {
-            title: "Хөдөлгүүр",
-            value: "2500 cc",
-          },
-        ],
-      },
-      {
-        title: "2014 TOYOTA COROLLA FIELDER",
-        image:
-          "https://8.ajes.com/imgs/2hSsOwl3qzivoAuAqJCgC8gJ1J9UiOzImMQUWr3fIgev4qu6mlwnRhV&w=320#https://8.ajes.com/imgs/2hSsOwl3qzivoAuAqJCgC8gJ1J9UiOzImMQVcVPIb73B7GPNAw6oftk&h=50",
-        mainSpec: "S",
-        link: "/auction/oTqyOK8MrAxdNQ8",
-        subSpecs: [
-          {
-            title: "Өнгө",
-            value: "Silver",
-          },
-          {
-            title: "Хөдөлгүүр",
-            value: "2500 cc",
-          },
-        ],
-      },
+      // {
+      //   title: "2014 Toyota Harrier",
+      //   image:
+      //     "https://8.ajes.com/imgs/23FCi0VbnvRhXb4iHollYQZOsAFE37O8hY7Ix48a8C&w=320#https://8.ajes.com/imgs/23FCi0VbnvRhXb4iHollYQZOsAFE37O968IR6F4eEp&h=50",
+      //   mainSpec: "3.5",
+      //   link: "/auction/oTqyOK8MrAxdNQ8",
+      //   subSpecs: [
+      //     {
+      //       title: "Өнгө",
+      //       value: "Silver",
+      //     },
+      //     {
+      //       title: "Хөдөлгүүр",
+      //       value: "2500 cc",
+      //     },
+      //   ],
+      //   originalItem: {
+      //     AUCTION: "TAA Kinki",
+      //     AUCTION_DATE: "2020-12-22 00:00:00",
+      //     AVG_PRICE: "1260000",
+      //     AVG_STRING: "1280,1242,1259",
+      //     COLOR: "silver",
+      //     ENG_V: "2000",
+      //     EQUIP: "AAC",
+      //     FINISH: "994000",
+      //     GRADE: "S",
+      //     ID: "JlcFVEmcTsMPaw",
+      //     IMAGES:
+      //       "https://8.ajes.com/imgs/dyJ2O5ZnVGaLpjCgSr4JtiXZqMvjmvoi6QPanFbXeO&h=50#https://8.ajes.com/imgs/dyJ2O5ZnVGaLpjCgSr4JtiXZqMvjmvoiT1riWh92LB&h=50",
+      //     KPP: "IAT",
+      //     KPP_TYPE: "2",
+      //     KUZOV: "RK5",
+      //     LOT: "2092",
+      //     MARKA_ID: "5",
+      //     MARKA_NAME: "HONDA",
+      //     MILEAGE: "32000",
+      //     MODEL_ID: "621",
+      //     MODEL_NAME: "STEP WAGON",
+      //     PRIV: "",
+      //     PW: "",
+      //     RATE: "3.5",
+      //     START: "550000",
+      //     STATUS: "Sold",
+      //     TIME: "2020-12-22 14:48:54",
+      //     YEAR: "2014",
+      //   },
+      // },
+      // {
+      //   title: "2014 Toyota Harrier",
+      //   image:
+      //     "https://8.ajes.com/imgs/aKxY8uthGoTnVCYdz2ga1hRTLFvzLVor4ms1bIBEOyTspOf0dsb3&w=320#https://8.ajes.com/imgs/aKxY8uthGoTnVCYdz2ga1hRTLFvzLVor4ms1bIBEOyTspOqISQTv&h=50",
+      //   mainSpec: "3.5",
+      //   link: "/auction/oTqyOK8MrAxdNQ8",
+      //   subSpecs: [
+      //     {
+      //       title: "Өнгө",
+      //       value: "Silver",
+      //     },
+      //     {
+      //       title: "Хөдөлгүүр",
+      //       value: "2500 cc",
+      //     },
+      //   ],
+      //   originalItem: {
+      //     AUCTION: "HERO",
+      //     AUCTION_DATE: "2020-12-23 12:07:00",
+      //     AVG_PRICE: "1312000",
+      //     AVG_STRING: "1416,1236,1240,1357",
+      //     COLOR: "white",
+      //     ENG_V: "2000",
+      //     EQUIP: "AC",
+      //     FINISH: "0",
+      //     GRADE: "GTI DR",
+      //     ID: "IClmlGOTGnOiug",
+      //     IMAGES:
+      //       "https://8.ajes.com/imgs/8KvxD4CPeYJRFogAQq34T4hmcYi2zLzeXXBpzmWKV5iT&h=50#https://8.ajes.com/imgs/8KvxD4CPeYJRFogAQq34T4hmcYi2zLzeXXBpzpneBfCY&h=50",
+      //     KPP: "FA",
+      //     KPP_TYPE: "2",
+      //     KUZOV: "AUCHH",
+      //     LOT: "2017",
+      //     MARKA_ID: "31",
+      //     MARKA_NAME: "VOLKSWAGEN",
+      //     MILEAGE: "31000",
+      //     MODEL_ID: "1053",
+      //     MODEL_NAME: "GOLF",
+      //     PRIV: "",
+      //     PW: "",
+      //     RATE: "4",
+      //     START: "100000",
+      //     STATUS: "",
+      //     TIME: "2020-12-22 16:14:23",
+      //     YEAR: "2014",
+      //   },
+      // },
+      // {
+      //   title: "2014 TOYOTA COROLLA FIELDER",
+      //   image:
+      //     "https://8.ajes.com/imgs/2hSsOwl3qzivoAuAqJCgC8gJ1J9UiOzImMQUWr3fIgev4qu6mlwnRhV&w=320#https://8.ajes.com/imgs/2hSsOwl3qzivoAuAqJCgC8gJ1J9UiOzImMQVcVPIb73B7GPNAw6oftk&h=50",
+      //   mainSpec: "S",
+      //   link: "/auction/oTqyOK8MrAxdNQ8",
+      //   subSpecs: [
+      //     {
+      //       title: "Өнгө",
+      //       value: "Silver",
+      //     },
+      //     {
+      //       title: "Хөдөлгүүр",
+      //       value: "2500 cc",
+      //     },
+      //   ],
+      //   originalItem: {
+      //     AUCTION: "MIRIVE Osaka",
+      //     AUCTION_DATE: "2020-12-24 11:40:00",
+      //     AVG_PRICE: "1903000",
+      //     AVG_STRING: "1860,1877,2250,1512,2138,2122,1569,2149,2124,2041",
+      //     COLOR: "silver",
+      //     ENG_V: "2500",
+      //     EQUIP: "AAC",
+      //     FINISH: "0",
+      //     GRADE: "Hybrid Royal",
+      //     ID: "32Jw0j1uGY4YlaD",
+      //     IMAGES:
+      //       "https://8.ajes.com/imgs/8KvxD4CPeYJMMMTdK3cmW9nh9raYZJSQuOsas8Sb6i71&h=50#https://8.ajes.com/imgs/8KvxD4CPeYJMMMTdK3cmW9nh9raYZJSQuOsasaFO7gR2&h=50",
+      //     KPP: "FAT",
+      //     KPP_TYPE: "2",
+      //     KUZOV: "AWS210",
+      //     LOT: "70048",
+      //     MARKA_ID: "1",
+      //     MARKA_NAME: "TOYOTA",
+      //     MILEAGE: "61000",
+      //     MODEL_ID: "60",
+      //     MODEL_NAME: "CROWN",
+      //     PRIV: "",
+      //     PW: "",
+      //     RATE: "4",
+      //     START: "850000",
+      //     STATUS: "",
+      //     TIME: "2020-12-22 13:40:36",
+      //     YEAR: "2014",
+      //   },
+      // },
     ],
     loading: false,
     error: "",
