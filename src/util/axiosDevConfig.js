8c8fe39c5585def96ce0e2ba3b903dce544d82f5
Jargal <jargal@kt.mn>
2021-01-25 22:07:48 +0800

    dev axios config

--- src/util/axiosDevConfig.js
+++ src/util/axiosDevConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://172.169.88.11:8080/erp-services/RestWS/runJson",
+  // baseURL: "http://172.169.88.11:8080/erp-services/RestWS/runJson",
+  baseURL: "https://dev.veritech.mn:8181/erp-services/RestWS/runJson",
 });
 
 export default instance;
